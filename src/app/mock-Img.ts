d89cc2f2fef2e448a3792ff0115c402a7d8aa449
LucassCassaretto <lucascassaretto2@gmail.com>
2024-09-20 21:06:04 -0300

    miniv2

--- src/app/mock-Img.ts
+++ src/app/mock-Img.ts
@@ -4,6 +4,9 @@ export const img_card: Img[] = [
 
     new Img(1,"img/100.jpg"),
     new Img(2,"img/101.jpg"),
-    new Img(3,"img/102.jpg"),
-   
+    new Img(3,"img/1007.jpg"),  
+    new Img(4,"img/103.jpg"),
+    new Img(5,"img/104.jpg"),
+    new Img(6,"img/105.jpg"),
+
 ]
